259eee64d18609d2ed5ae3a816d676d6ee78a669
emanuelbrits <emanuelbritoss@outlook.com>
2025-03-01 13:16:47 -0300

    criada documentacao

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -6,9 +6,4 @@ import { ApiKeyGuard } from './api-key-guard/api-key.guard';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @UseGuards(ApiKeyGuard)
-  getHello(): string {
-    return this.appService.getHello();
-  }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,13 +2,24 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Permitir JSON no corpo da requisição
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Gerenciador de Pedidos')
+    .setDescription('Documentação da API para gerenciamento de pedidos')
+    .setVersion('1.0')
+    .addBearerAuth() // Se usar JWT
+    .addApiKey({ type: 'apiKey', name: 'x-api-key', in: 'header' }) // Se usar API Key
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
